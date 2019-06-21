8eba6112288dcae8d617ea1b04f34de00a31af1d
Tyler <tylerh.atkinson@gmail.com>
2019-06-21 12:32:28 -0400

    Added id variable

--- MTGCard.py
+++ MTGCard.py
@@ -1,14 +1,17 @@
 from datetime import datetime
+
+
 class MTGCard:
-    def __init__(self, name, release_date, oracle_text, set_name, url):
+    def __init__(self, name, release_date, oracle_text, set_name, url, id):
         self.name = name
         self.release_date = datetime.strptime(release_date, '%Y-%m-%d').date()
         self.oracle_text = oracle_text
         self.set_name = set_name
         self.url = url
+        self.id = id
 
 
 def parse_card_from_map(card_map):
     if 'oracle_text' in card_map:
-        return MTGCard(card_map['name'], card_map['released_at'], card_map['oracle_text'], card_map['set'], card_map['scryfall_uri'])
-    return MTGCard(card_map['name'], card_map['released_at'], None, card_map['set'], card_map['scryfall_uri'])
+        return MTGCard(card_map['name'], card_map['released_at'], card_map['oracle_text'], card_map['set'], card_map['scryfall_uri'], card_map['id'])
+    return MTGCard(card_map['name'], card_map['released_at'], None, card_map['set'], card_map['scryfall_uri'], card_map['id'])
--- MTGSet.py
+++ MTGSet.py
@@ -2,14 +2,13 @@ from datetime import datetime
 
 
 class MTGSet:
-    def __init__(self, name, code, release_date, card_count, set_type, num_of_cards):
+    def __init__(self, name, code, release_date, card_count, set_type):
         self.name = name
         self.code = code
         self.release_date = datetime.strptime(release_date, '%Y-%m-%d').date()
         self.card_count = card_count
         self.set_type = set_type
-        self.num_of_cards = num_of_cards
 
 
 def parse_set_from_map(set_map):
-    return MTGSet(set_map['name'], set_map['code'], set_map['released_at'], set_map['card_count'], set_map['set_type'], set_map['card_count'])
+    return MTGSet(set_map['name'], set_map['code'], set_map['released_at'], set_map['card_count'], set_map['set_type'])
