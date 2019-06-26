0a3fed35f92de97581cf153b968bdd0559bb944c
Tyler <tylerh.atkinson@gmail.com>
2019-06-25 20:16:02 -0400

    Adding actual logic to the api

--- Api.py
+++ Api.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from MTGSpoilerBot import MTGSpoilerBot
 app = Flask(__name__)
 
 
 @app.route("/CheckForSets", methods=['GET'])
 def check_for_sets():
-    return "Hello World!"
+    bot = MTGSpoilerBot()
+    bot.check_for_new_sets()
+    return bot.current_sets
 
 
 @app.route("/CheckForCards/<mtg_set>", methods=['GET'])
 def check_for_cards(mtg_set):
-    return mtg_set
+    bot = MTGSpoilerBot()
+    bot.check_for_new_cards()
+    return bot.current_cards
 
