2a1473c345858f630dd1e6ca0486fb70f92713d9
Tyler <tylerh.atkinson@gmail.com>
2019-06-25 20:28:36 -0400

    Added Json serializer

--- Api.py
+++ Api.py
@@ -1,6 +1,35 @@
 from flask import Flask, jsonify
+from flask.json import JSONEncoder
 from MTGSpoilerBot import MTGSpoilerBot
+from MTGCard import MTGCard
+from MTGSet import MTGSet
+
 app = Flask(__name__)
+
+
+class MyJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, MTGCard):
+            return {
+                'Name': obj.name,
+                'ReleaseDate': obj.release_date,
+                'OracleTest': obj.oracle_text,
+                'SetName': obj.set_name,
+                'Url': obj.url,
+                'Id': obj.id
+            }
+        if isinstance(obj, MTGSet):
+            return {
+                'Name': obj.name,
+                'Code': obj.code,
+                'ReleaseDate': obj.release_date,
+                'CardCount': obj.card_count,
+                'SetType': obj.set_type
+            }
+        return super(MyJSONEncoder, self).default(obj)
+
+
+app.json_encoder = MyJSONEncoder
 
 
 @app.route("/CheckForSets", methods=['GET'])
@@ -15,4 +44,7 @@ def check_for_cards(mtg_set):
     bot = MTGSpoilerBot()
     bot.check_for_new_cards()
     return jsonify(bot.current_cards)
+
+
+
 
