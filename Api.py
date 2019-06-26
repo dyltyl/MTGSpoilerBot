e585665c33f00ed4361976b24b28d1560ab97f34
Tyler <tylerh.atkinson@gmail.com>
2019-06-25 20:10:12 -0400

    Created api

--- Api.py
+++ Api.py
@@ -2,7 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route("/CheckForSets", methods=['GET'])
+def check_for_sets():
     return "Hello World!"
+
+
+@app.route("/CheckForCards/<mtg_set>", methods=['GET'])
+def check_for_cards(mtg_set):
+    return mtg_set
 
